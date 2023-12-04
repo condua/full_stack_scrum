9288b4b7128d528e19c794b0d52e0fdb1e32489d
spitliner <>
2023-12-04 11:20:15 +0700

    add create dub account

--- __tests__/accountCreate.js
+++ __tests__/accountCreate.js
@@ -1,6 +1,19 @@
 const app = require('../server');
 const request = require('supertest');
 const User = require('../models/user');
+const mongoose = require('mongoose');
+
+// Set timeout to 10 seconds
+jest.setTimeout(10000);
+
+// Wait for 3 seconds before each test to make sure the database is ready
+beforeAll(async () => {
+  await new Promise((resolve) => setTimeout(() => resolve(), 3000));
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
 describe("createAccount", () => {
     test("Should newly created account", async () => {
@@ -28,6 +41,31 @@ describe("createAccount", () => {
 
         User.deleteOne({email: "testemail1@junkmail.com"})
     }, 9999)
+})
 
-    
+describe("createDubAccount", () => {
+    test("Should return error because email already in used", async () => {
+        await request(app)
+            .post('/register')
+            .send({
+                fullname: "Nguyen Van A",
+                email: "testemail1@junkmail.com",
+                password: "JvFWNB8B76XbRivJ",
+                role: "Teacher"
+            })
+
+        const createDupAccount = await request(app)
+            .post('/register')
+            .send({
+                fullname: "Nguyen Van B",
+                email: "testemail1@junkmail.com",
+                password: "tfqOPRmjtHKfIO5l",
+                role: "student"
+            })
+
+        expect(createDupAccount.statusCode).toEqual(400)
+        expect(createDupAccount.body.error).toEqual("Username is already taken");
+
+        User.deleteOne({email: "testemail1@junkmail.com"})
+    }, 9999)
 })
